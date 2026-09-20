ec09800ec1d6d97192f8ab93d273cea2cc9cde93
Swapnanil <sg.swapnanil72@gmail.com>
2026-09-21 00:17:58 +0530

    Update font settings and import in TextPressure component

--- my-react-app/src/components/Hero/TextPressure.js
+++ my-react-app/src/components/Hero/TextPressure.js
@@ -25,8 +25,8 @@ const debounce = (func, delay) => {
 
 const TextPressure = ({
   text = 'Compressa',
-  fontFamily = 'Compressa VF',
-  fontUrl = 'https://res.cloudinary.com/dr6lvwubh/raw/upload/v1529908256/CompressaPRO-GX.woff2',
+  fontFamily = '"Roboto Flex", sans-serif',
+  fontUrl = 'https://fonts.googleapis.com/css2?family=Roboto+Flex:opsz,wdth,wght@8..144,25..151,100..1000&display=swap',
 
   width = true,
   weight = true,
@@ -162,11 +162,7 @@ const TextPressure = ({
   const styleElement = useMemo(() => {
     return (
       <style>{`
-        @font-face {
-          font-family: '${fontFamily}';
-          src: url('${fontUrl}');
-          font-style: normal;
-        }
+        @import url('${fontUrl}');
 
         .flex {
           display: flex;
@@ -236,7 +232,7 @@ const TextPressure = ({
               color: stroke ? undefined : textColor
             }}
           >
-            {char}
+            {char === ' ' ? '\u00A0' : char}
           </span>
         ))}
       </h1>
